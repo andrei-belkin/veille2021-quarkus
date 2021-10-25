483461d6465915b2c030aa6530d656e9561f428d
Andrei Belkin <andrei.belkin@pm.me>
2021-10-24 23:59:41 -0400

    Generated empty Quarkus project

---
+++ src/test/java/qc/ca/claurendeau/belkinandrei/NativeVeilleQuarkusIT.java
@@ -0,0 +1,9 @@
+package qc.ca.claurendeau.belkinandrei;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeVeilleQuarkusIT extends VeilleQuarkusTest {
+
+    // Execute the same tests but in native mode.
+}
