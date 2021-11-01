492a52842ccecccec324d7d6cf297db211283d6f
Andrei Belkin <andrei.belkin@pm.me>
2021-11-01 10:07:51 -0400

    VQ-03 : Changé superclasse de PanacheEntity à PanacheEntityBase

--- src/main/java/qc/ca/claurendeau/belkinandrei/persistence/ResumeRepository.java
+++ src/main/java/qc/ca/claurendeau/belkinandrei/persistence/ResumeRepository.java
@@ -1,10 +1,11 @@
 package qc.ca.claurendeau.belkinandrei.persistence;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import qc.ca.claurendeau.belkinandrei.entity.Resume;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.UUID;
 
 @ApplicationScoped
-public class ResumeRepository implements PanacheRepository<Resume> {
+public class ResumeRepository implements PanacheRepositoryBase<Resume, UUID> {
 }
