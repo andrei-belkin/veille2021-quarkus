4b9fe1ed22bfe126d02e1bb6c307ad6172580f71
Andrei Belkin <andrei.belkin@pm.me>
2021-11-01 09:59:54 -0400

    VQ-01 : Créé entité, étendu PanacheEntityBase, supprimé annotation Entity

--- src/main/java/qc/ca/claurendeau/belkinandrei/entity/Resume.java
+++ src/main/java/qc/ca/claurendeau/belkinandrei/entity/Resume.java
@@ -1,19 +1,22 @@
 package qc.ca.claurendeau.belkinandrei.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.*;
 import lombok.Builder.Default;
 import qc.ca.claurendeau.belkinandrei.util.ReviewState;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Builder(toBuilder = true)
-@Entity
-public class Resume {
+public class Resume extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
@@ -24,5 +27,4 @@ public class Resume {
     private ReviewState reviewState = ReviewState.PENDING;
     private String reasonForRejection;
     private UUID ownerId;
-//    private List<UUID> studentApplications;
 }
